92e2dae825e5566640799181067ea600e294466b
nvanha <nvha1120@gmail.com>
2023-01-04 15:03:44 +0700

    Setup Amplify

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
+import { Amplify } from "aws-amplify";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import config from "./aws-exports";
 import "./styles/index.scss";
+
+Amplify.configure(config);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
